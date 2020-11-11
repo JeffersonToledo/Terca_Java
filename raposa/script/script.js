86be118afca4459dfbba76b6c8eb2d87562c69dd
JeffersonToledo <abelktoledo@gmail.com>
2020-11-10 21:12:09 -0300

    primeiro commit

---
+++ raposa/script/script.js
@@ -0,0 +1,22 @@
+const animais = document.getElementById('animais');
+console.log (animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection[0]);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const LinkExerteno = document.querySelector('[href^="#"]');
+console.log(LinkExerteno);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg);
+
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('grid-section');
+
+primeiraUl.classList.add('grid-section');
+
+console.log(gridSectionHTML);
+console.log(gridSectionNode);
